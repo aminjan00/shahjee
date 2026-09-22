564d93441649ab37b28f42ae744054eb38f0bb1e
aminjan00 <studentsky67@gmail.com>
2026-09-22 10:02:14 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
     /* ==========================================
         3. HERO SLIDER & DYNAMIC BANNERS
     ========================================== */
-    const BANNER_API_URL = "http://localhost:5000/api/banners";
+    const BANNER_API_URL = "https://shahjee-production-9193.up.railway.app/api/banners";
 
     const slidesWrapper = document.getElementById('hero-slides-wrapper');
     const prevBtn = document.getElementById('slider-prev');
