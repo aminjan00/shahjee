56e48a9c418e4b7c63b19db58a9e86b74f57a130
aminjan00 <studentsky67@gmail.com>
2026-09-22 06:29:57 -0700

    Delete shahjee-backend directory

--- shahjee-backend/models/Banner.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const bannerSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-  linkUrl: { type: String, default: '#' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Banner', bannerSchema);
