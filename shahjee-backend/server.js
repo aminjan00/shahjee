389f2b0a56c4b087c87199bf2e29bd5f4866aeca
aminjan00 <studentsky67@gmail.com>
2026-09-22 02:20:24 -0700

    Update server.js

--- shahjee-backend/server.js
+++ shahjee-backend/server.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+// 👇 YEH LINE ADD KAREIN
+app.use(express.static(__dirname));
 
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/banners', require('./routes/bannerRoutes'));
