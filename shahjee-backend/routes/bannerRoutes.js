dbee63a2d7d3448dcf1d9afdd218946118219aa6
aminjan00 <studentsky67@gmail.com>
2026-09-22 22:04:22 -0700

    Delete shahjee-backend directory

--- shahjee-backend/routes/bannerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBanners, createBanner, deleteBanner } = require('../controllers/bannerController');
-
-router.route('/').get(getBanners).post(createBanner);
-router.route('/:id').delete(deleteBanner);
-
-module.exports = router;
